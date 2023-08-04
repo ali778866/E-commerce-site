2141605d2500c8a097f14a9cf5244c4008455006
ali778866 <mdali.ali76@gmail.com>
2023-08-04 11:06:17 +0530

    controllers added

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', productsController.getAddProduct);
+
+router.post('/add-product', productsController.postAddProduct);
+
+exports.routes = router;
+exports.products = products;
