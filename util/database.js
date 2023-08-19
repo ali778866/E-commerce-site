f47821f7b23e8a7553dd6e374dc30526e59544d3
ali778866 <mdali.ali76@gmail.com>
2023-08-19 21:52:15 +0530

    sequelize installed and model/product.js updated

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost', // your database server address here,
-    user: 'root', 
-    database: 'node-new',
-    password: 'Ali@9934'
-})
+const sequelize = new Sequelize(
+    'node-new',
+    'root',
+    'Ali@9934', 
+    {dialect: 'mysql',  host: 'localhost'}
+)
 
-module.exports = pool.promise();
+module.exports = sequelize;
